cbf2abec879e166cfa45d88193782b43c5024189
Txny <irakozeyves9@gmail.com>
2023-01-24 19:08:28 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+export default app;

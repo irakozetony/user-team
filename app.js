269b1c7c99460959fa0c966276e3c44aea94336d
Txny <irakozeyves9@gmail.com>
2023-01-25 22:11:22 +0200

    bg-fix-db-connection

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
-
+import { sequelize } from "./database/models/";
 dotenv.config();
 
 const app = express();
+
+export const connectDb = async ()=>{
+    console.log("Checking database connection...");
+
+    try{
+        await sequelize.authenticate();
+        console.log("Database connection established");
+    }
+    catch(err){
+        console.log(`Database connection failed: ${err}`);
+        process.exit(1);
+    }
+}
 
 app.use(morgan("dev"));
 app.use(express.json());
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import app from "./app";
 import dotenv from "dotenv";
+import { connectDb } from "./app";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening on port ${process.env.PORT}`);
-});
+(async () => {
+    await connectDb();
+    app.listen(process.env.PORT, () => {
+        console.log(`Server listening on port ${process.env.PORT}`);
+    });
+})();

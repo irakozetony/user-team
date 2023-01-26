237f70630971e74708ee02942cfb1f8bf4b01a95
ndatumuremyi <paternenught@gmail.com>
2023-01-26 12:33:56 +0200

    add google auth

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import { sequelize } from "./database/models/";
 import passport from "passport";
+import session from 'express-session';
+import {ensureAuth,ensureGuest} from './google/middlewares'
+import './google/auth';
 import { authenticate, localSignupStrategy } from "./auth/localAuth";
 import localAuthRouter from "./routes/localAuthRoute";
 import updateRoute from "./routes/updateProfile";
@@ -11,6 +14,10 @@ import models from './database/models/index';
 dotenv.config();
 
 const app = express();
+app.use(session({secret:'dats',resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session({secret:'dats',resave: true, saveUninitialized: true}));
+
 
 export const connectDb = async ()=>{
     console.log("Checking database connection...");
@@ -40,5 +47,26 @@ app.use("/api/users", async(req, res)=>{
     catch(err){console.log(err)}
 })
 
-
+app.get('/auth/failure',(req,res)=>res.send('Something went wrong...'))
+app.get('/auth/google',
+    passport.authenticate('google',{scope:['email','profile']})
+)
+app.get('/google/callback',
+    passport.authenticate('google',{
+        successRedirect:'/protected',
+        failureRedirect: '/auth/failure'
+    })
+)
+app.get('/sample-ui',ensureGuest,(req,res)=>{
+    res.send(`<a href="/auth/google">Authenticate with google </a>`);
+})
+app.get('/protected',ensureAuth,(req,res)=>{
+    console.log("user:",req.user)
+    res.send(`Hello !,${JSON.stringify(req.user)}`);
+})
+app.get('/logout',(req,res)=>{
+    req.logout();
+    req.session.destroy();
+    res.send('GoodBye')
+})
 export default app;
